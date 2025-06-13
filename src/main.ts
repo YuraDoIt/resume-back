ddb87567cff7bc53e9c464551e9405b77118f1d2
YuraDoIt <yura9989@gmail.com>
2025-06-13 13:53:23 +0300

    add libraries and packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common"
+import { NestFactory } from "@nestjs/core"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import * as dotenv from "dotenv"
+
+import { AppModule } from "./app.module"
+
+dotenv.config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+	const PORT = process.env.PORT
+
+	const config = new DocumentBuilder()
+		.setTitle("API Documentation")
+		.setDescription("The API description")
+		.setVersion("1.0")
+		.addBearerAuth(
+			{
+				type: "http",
+				scheme: "bearer",
+				bearerFormat: "JWT",
+				name: "Authorization",
+				in: "header"
+			},
+			"access-token"
+		)
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup("api", app, document)
+
+	await app.listen(PORT ?? 3001, () => {
+		console.log(
+			`Server started on port http://localhost:${process.env.PORT}/api`
+		)
+	})
 }
-bootstrap();
+bootstrap()
